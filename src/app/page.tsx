09b005eabfa4ddb6ffda8d710f31ba95837a03ef
TahirHameed74 <tsb_125@yahoo.com>
2024-03-26 23:55:28 +0500

    changes on section 1,2,3,4,5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,14 +43,14 @@ export default function Home() {
 			address: "A General Dentist in Houston, TX",
 			description:
 				"As a busy dentist, patient engagement is crucial yet time-consuming. With the help of the AI chatbot, we’ve streamlined appointment scheduling, dental inquiries, & follow-up care. This technology has incredibly reduced our administrative workload, allowing us to focus more on providing top-notch oral care to our patients. It’s like having an extra pair of hands that work 24/7!",
-			imagen: "../images/drV.png",
+			imagen: "images/drV.png",
 		},
 		{
 			client: "Dr. Tran and Dr. Nguyen",
 			address: "Cosmetic Dentists Located in Memorial City, TX",
 			description:
 				"Integrating an AI chatbot into our dental practice has dramatically enhanced our patient service. It instantly addresses patient questions, offering them convenient and comprehensive responses. Our patients are happier with the quick, around-the-clock assistance, and our staff is relieved from the constant ringing of phones. It’s a win-win solution for us!",
-			imagen: "../images/drT.png",
+			imagen: "images/drT.png",
 		},
 	];
 
